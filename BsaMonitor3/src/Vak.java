a575a85245d0a6fd0f9d7ba9a8239402f2232335
Koen Lippe <koen.lippe@hva.nl>
2018-11-21 11:32:36 +0100

    BsaMonitor3 added

---
+++ BsaMonitor3/src/Vak.java
@@ -0,0 +1,38 @@
+/**
+ * Description:
+ *
+ * @author Koen Lippe (500794493)
+ *
+ */
+
+public class Vak {
+    //Variables
+    public String name;
+    public int punten;
+    public double cijfer;
+
+    //Constructor
+
+
+
+    //Methods
+    public String getNaam(){
+        return this.name;
+    }
+
+    public int getPunten(){
+        return punten;
+    }
+
+    public double getCijfer(){
+        return 0;
+    }
+
+    public void setCijfer(double nieuwCijfer) {
+        this.cijfer = nieuwCijfer;
+    }
+
+    public int gehaaldePunten(){
+
+    }
+}
