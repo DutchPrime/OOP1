d9133ef6144cb26d2fcb4bdd21e34a15ae124135
Koen Lippe <koen.lippe@hva.nl>
2018-11-22 11:26:09 +0100

    Fresh Kofferslot start

--- Kofferslot 3a/src/Kofferslot.java
+++ Kofferslot 3a/src/Kofferslot.java
@@ -32,28 +32,12 @@ public class Kofferslot {
     public void volgende(){
         //Number is not 9 yet so increase number
         if(this.cijfer[0].getCijfer() < 9){
-            this.cijfer[0].setCijfer(0);
+            this.cijfer[0].volgende();
         }
         //Number = 9 so increase some letters.
         else if(this.cijfer[0].getCijfer() == 9){
 
-            //bot letters are 'Z' --> number to 0, both letters to 'A'
-            if(letterlijst[1].getLetter() == 'Z' && letterlijst[0].getLetter() == 'Z'){
-                this.cijfer[0].setCijfer(0);
-                letterlijst[0].setLetter('A');
-                letterlijst[1].setLetter('A');
-            }
-            //Only second letter = 'Z' --> set second letter tp 'A', increase second number. set number to 0 (number = 9)
-            else if (letterlijst[1].getLetter() == 'Z'){
-                letterlijst[1].setLetter('A');
-                letterlijst[0].volgende();
-                this.cijfer[0].setCijfer(0);
-            }
-            //Only number is 9 so set number to 0 and increase second number
-            else {
-                this.cijfer[0].setCijfer(0);
-                letterlijst[1].volgende();
-            }
+            
 
         }
 
